23249cc5626b7666b35854307ca4beeea77c56b2
David B Mittelman <dbmittelman@gmail.com>
2016-06-15 21:15:51 -0700

    Seed database on startup, if needed

---
+++ imports/api/calls/calls.js
@@ -0,0 +1,17 @@
+import { Mongo } from 'meteor/mongo';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+
+class CallsCollection extends Mongo.Collection {
+
+}
+
+export const Calls = new CallsCollection('Calls');
+
+Calls.schema = new SimpleSchema({
+  timestamp: { type: Number },
+  clientName: { type: String },
+  endpointCalled: { type: String },
+  result: { type: String, allowedValues: ['success', 'error'] },
+});
+
+Calls.attachSchema(Calls.schema);
