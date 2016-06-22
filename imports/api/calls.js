797e99bd2a9b2efe06dcad172bbfd3060eaa2ccf
David B Mittelman <dbmittelman@gmail.com>
2016-06-22 15:29:50 -0400

    Allow inserts

--- imports/api/calls.js
+++ imports/api/calls.js
@@ -1,10 +1,16 @@
 import { Mongo } from 'meteor/mongo';
 
-export const Calls = new Mongo.Collection('calls');
+export let Calls = new Mongo.Collection('calls');;
 
 if (Meteor.isServer) {
   // This code only runs on the server
   Meteor.publish('calls', function callsPublication(limit) {
     return Calls.find({}, { limit: limit, sort: { timestamp: -1 } });
+  });
+
+  Calls.allow({
+    insert: function (userId, doc, fields, modifier) {
+      return true;
+    },
   });
 }
